68c58b2077c87e36ce211badc623712f645ad76d
5tr1ker <tjseocld@naver.com>
2023-01-03 01:35:00 +0900

    feat: 프로그램 시작시 프로그램 자동 실행 구현
    프로그램 시작시 프로그램 자동 실행 구현

--- main.cpp
+++ main.cpp
@@ -10,3 +10,26 @@ void main() {
 	comonResourceManagement::resourceInfo form;
 	Application::Run(% form);
 }
+
+/*
+void cs_util_register_start_Prog()
+{
+#include "atlbase.h"
+#include <direct.h>
+
+	char* pstrBuffer = NULL;
+	char strBuffer[_MAX_PATH] = { };
+	pstrBuffer = getcwd(strBuffer, _MAX_PATH);
+	strcat_s(strBuffer, "\\comonResourceManagement.exe");
+
+	CRegKey RegKey;
+	wchar_t wtext[255];
+	mbstowcs(wtext, strBuffer, strlen(strBuffer) + 1);
+	LPWSTR ptr = wtext;
+
+	cout << strBuffer;
+	RegKey.Create(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+	RegKey.SetValue(ptr, L"itman");
+	RegKey.Close();
+}
+*/
